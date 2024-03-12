60c39a5a63a29c7f4188b3e5dd025174f0f6c342
SmValeriya <lerok1000@gmail.com>
2024-03-12 21:33:22 +0200

    fix fonts for creating woff

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -1,6 +1,7 @@
 import gulp from "gulp";
 import { existsSync } from "fs";
 import fonter from "gulp-fonter";
+import ttf2woff from "gulp-ttf2woff";
 import ttf2woff2 from "gulp-ttf2woff2";
 import plumber from "gulp-plumber";
 import { paths } from "../config/paths.js";
@@ -18,6 +19,9 @@ export const fonts = () => {
       `!${paths.fonts.src}*.{woff,woff2,ttf}`])
       .pipe(plumber())
       .pipe(fonter({ formats: ['ttf', 'woff'] }))
+      .pipe(dest(paths.fonts.src))
+      .pipe(src(`${paths.fonts.src}*.ttf`))
+      .pipe(ttf2woff())
       .pipe(dest(paths.fonts.src))
       .pipe(src(`${paths.fonts.src}*.ttf`))
       .pipe(ttf2woff2())
